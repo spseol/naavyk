31faa3c1f61ba8e4b8859b3663c5ffaf7ffe6b83
Marrek Nožka <marek@tlapicka.net>
2020-11-09 16:12:59 +0100

    přidáno Potvrzení před smazáním položky nebo skupyny.

--- webface/templates/myajax.js
+++ webface/templates/myajax.js
@@ -1,42 +1,40 @@
+responseProcess = function(data) {
+    alert(data);
+}
+
+updateClick = function(event) {
+    event.preventDefault();
+    //$(this).parents('ul').css('color', 'red');
+    form = $(this).parents('form');
+    //alert(form[0].tagName)
+    var fdata = new FormData(form[0]);
+    //fdata['gid'] = '{{ gid }}';
+    //alert(fdata);
+    var url = "{{ url_for('orderAJAX', gid=gid) }}";
+    $.ajax({
+        url: url,
+        data: fdata,
+        cache: false,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function (confirmdata) {
+            var input = $(form).find('input.count');
+            input.val(confirmdata.count);
+            $('#total-price').text(confirmdata.price+',-');
+            $('#total-count').text(confirmdata.totalcount);
+            input.parent().append('<span id="donedone" >&#128504;</span>')
+            $('#donedone').fadeOut(800);
+            setTimeout(function() {
+                $('#donedone').remove();
+            }, 801);
+
+        }
+    });
+};
+
 
 $( document ).ready(function() {
-
-    responseProcess = function(data) {
-        alert(data);
-    }
-
-    updateClick = function(event) {
-        event.preventDefault();
-        //$(this).parents('ul').css('color', 'red');
-        form = $(this).parents('form');
-        //alert(form[0].tagName)
-        var fdata = new FormData(form[0]);
-        //fdata['gid'] = '{{ gid }}';
-        //alert(fdata);
-        var url = "{{ url_for('orderAJAX', gid=gid) }}";
-        $.ajax({
-            url: url,
-            data: fdata,
-            cache: false,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            success: function (confirmdata) {
-                var input = $(form).find('input.count');
-                input.val(confirmdata.count);
-                $('#total-price').text(confirmdata.price+',-');
-                $('#total-count').text(confirmdata.totalcount);
-                input.parent().append('<span id="donedone" >&#128504;</span>')
-                $('#donedone').fadeOut(800);
-                setTimeout(function() {
-                    $('#donedone').remove();
-                }, 801);
-
-            }
-        });
-    };
-
     $('.update').click(updateClick);
-
 });
 
