87b1ddb3e0e7df6f0fc63fc63029d569c69f5c99
Marrek Nožka <marek@tlapicka.net>
2020-11-14 22:10:21 +0100

    podpora pro Markdown, zobrazení URL zkráceno

--- webface/__init__.py
+++ webface/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_pony import Pony
 from flask_login import LoginManager
+from flask_misaka import Misaka
 
 # from flask_misaka import Misaka
 from flask_wtf.csrf import CSRFProtect
@@ -13,6 +14,7 @@ app.config.from_object("config")
 app.config.from_pyfile("config.py", silent=True)
 
 pony = Pony(app)
+misaka = Misaka(app, escape=True)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"  # funkce pro url_for
 login_manager.login_message = "Nejprve je třeba se přihlásit :-)"
